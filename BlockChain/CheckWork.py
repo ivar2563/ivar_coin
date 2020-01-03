41f13143b4ea0d9b820476774f81a90232865441
e217822 <ivar.ollestad@evry.com>
2020-01-03 13:44:00 +0100

    Fixed the validate function

--- BlockChain/CheckWork.py
+++ BlockChain/CheckWork.py
@@ -5,11 +5,16 @@ import pickle
 def validate(string_):
     print("start")
     if is_valid(string_):
-        with open('proof_of_work_list.txt', 'rb') as fp:
-            list_ = pickle.load(fp)
+        if check_if_empty() is False:
+            with open('proof_of_work_list.txt', 'rb') as fp:
+                list_ = pickle.load(fp)
+                print(list_)
+        if check_if_empty() is True:
+            list_ = []
         if string_ not in list_:
+            list_.append(string_)
             with open("proof_of_work_list.txt", "wb") as fp:
-                pickle.dump(string_, fp)
+                pickle.dump(list_, fp)
             print(True)
             return True
         else:
@@ -18,3 +23,12 @@ def validate(string_):
     else:
         print(False)
         return False
+
+
+def check_if_empty():
+    try:
+        with open('proof_of_work_list.txt', 'rb') as fp:
+            list_ = pickle.load(fp)
+        return False
+    except EOFError:
+        return True
--- BlockChain/Test001.py
+++ BlockChain/Test001.py
@@ -5,14 +5,16 @@ import json
 email = "Ola.norman@lyse.net"
 bits = 20
 
-
+list_ = []
 
 
 def test():
     while True:
+        x = generate(email, bits)
+        list_.append(x)
         data = {
             "data": [2, "pro", {"x23": "2s"}, ["sda", 242]],
-            "string": generate(email, bits)
+            "string": x
         }
         x = requests.post("http://127.0.0.1:5000/api/add_node/", json=data)
         if x.status_code == 200:
@@ -22,5 +24,18 @@ def test():
     print(x)
 
 
-for a in range(100):
+def test2():
+    for a in list_:
+        data = {
+            "data": [2, "pro", {"x23": "2s"}, ["sda", 242]],
+            "string": a
+        }
+        x = requests.post("http://127.0.0.1:5000/api/add_node/", json=data)
+        if x.status_code == 200:
+            print(x.content)
+
+
+for a in range(10):
     test()
+
+test2()
