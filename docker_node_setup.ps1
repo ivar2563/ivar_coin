25e30a4644aef5cc4d4137adcc98804c272e5f31
ivarh <ivar.ollestad@evry.com>
2020-04-24 16:26:21 +0200

    Removed flask script and replaced it with threading. still needs a bit more work

--- docker_node_setup.ps1
+++ docker_node_setup.ps1
@@ -1,9 +1,10 @@
 ﻿$nodes = 3
-for ($i=0; $i -lt $nodes ;$i++){
-docker run -d -p "808$($i):5000" --mount target=/app/IvarCoin/Data ivarcoin:latest
+
+# Create first node
+docker run -d -p "8080:5000" --mount target=/app/IvarCoin/Data ivarcoin:latest
+
+# Create subsequent nodes
+for ($i=1; $i -lt $nodes ;$i++){
+  # Subsequent nodes will connect to first node
+  docker run -d -p "808$($i):5000" -e "REMOTE_PORT=8000" --mount target=/app/IvarCoin/Data ivarcoin:latest
 }
-
-
-
-#--mount source="ivarcoin$($i)-vol",target=/app/IvarCoin/Data ivarcoin:latest
-    
